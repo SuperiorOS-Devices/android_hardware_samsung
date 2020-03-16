591ab407fc9748e5e862faa3994a84f0599f63e8
Joshua Primero <jprimero155@gmail.com>
2020-03-16 14:03:36 +0000

    hw/samsung: doze: import needed resources
    * import: https://github.com/LineageOS/android_packages_resources_devicesettings/commit/95f475ebf501b4be6cfe7a80123e519eea213139
    
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- doze/Android.mk
+++ doze/Android.mk
@@ -22,7 +22,7 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/doze-res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
